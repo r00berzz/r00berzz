5d348e49c2afeccd9a2fceaeeef2ce08c785ee06
r00berzz <rodan.pluto@gmail.com>
2024-06-10 12:12:07 +0200

    Update r00berzz.lua

--- r00berzz.lua
+++ r00berzz.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/r00berzz/r00berzz/raw/r00berzz.lua'))()
+local Rayfield = loadstring(game:HttpGet('https://[Log in to view URL]'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Je n'ai pas de nom pour le hub",
@@ -34,3 +34,149 @@ Rayfield:Notify({
    Content = "Pas d'inspi",
    Duration = 5,
    Image = nil,
+   Actions = { -- Notification Buttons
+      Ignore = {
+         Name = "azé",
+         Callback = function()
+         print("The user tapped azé")
+      end
+   },
+},
+})
+
+local Button = MainTab:CreateButton({
+   Name = "Crtl+Click tp",
+   Callback = function()
+   -- The function that takes place when the button is pressed
+            local UIS = game:GetService("UserInputService")
+ 
+local Player = game.Players.LocalPlayer
+local Mouse = Player:GetMouse()
+ 
+ 
+function GetCharacter()
+   return game.Players.LocalPlayer.Character
+end
+ 
+function Teleport(pos)
+   local Char = GetCharacter()
+   if Char then
+       Char:MoveTo(pos)
+   end
+end
+ 
+ 
+UIS.InputBegan:Connect(function(input)
+   if input.UserInputType == Enum.UserInputType.MouseButton1 and UIS:IsKeyDown(Enum.KeyCode.LeftControl) then
+       Teleport(Mouse.Hit.p)
+   end
+end)
+   end,
+})
+
+local infJumpEnabled = false
+
+local Button = MainTab:CreateButton({
+    Name = "Infinite Jump",
+    Callback = function()
+        infJumpEnabled = not infJumpEnabled
+        if infJumpEnabled then
+            game:GetService("UserInputService").jumpRequest:Connect(function()
+                if infJumpEnabled then
+                    game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+                end
+            end)
+        else
+            game:GetService("UserInputService"):UnbindFromKey(Enum.KeyCode.Space)
+        end
+    end
+})
+
+
+print("Script started")
+
+local function waitForHumanoid(character)
+    while not character:FindFirstChild("Humanoid") do
+        wait(0.1)
+    end
+    return character.Humanoid
+end
+
+local function setWalkSpeed(Value)
+    print("Callback called with value: " .. Value)
+    local player = game.Players.LocalPlayer
+    if player and player.Character then
+        local humanoid = waitForHumanoid(player.Character)
+        if humanoid then
+            print("Setting WalkSpeed to: " .. Value)
+            humanoid.WalkSpeed = Value
+        else
+            print("Humanoid not found")
+        end
+    else
+        print("Player or Character not found")
+    end
+end
+
+local player = game.Players.LocalPlayer
+player.CharacterAdded:Connect(function(character)
+    waitForHumanoid(character) -- S'assurer que le Humanoid est prêt quand le personnage est ajouté
+end)
+
+print("Script started")
+
+local function waitForHumanoid(character)
+    while not character:FindFirstChild("Humanoid") do
+        wait(0.1)
+    end
+    return character.Humanoid
+end
+
+local function setWalkSpeed(Value)
+    print("Callback called with value: " .. Value)
+    local player = game.Players.LocalPlayer
+    if player and player.Character then
+        local humanoid = waitForHumanoid(player.Character)
+        if humanoid then
+            print("Setting WalkSpeed to: " .. Value)
+            humanoid.WalkSpeed = Value
+        else
+            print("Humanoid not found")
+        end
+    else
+        print("Player or Character not found")
+    end
+end
+
+local player = game.Players.LocalPlayer
+player.CharacterAdded:Connect(function(character)
+    waitForHumanoid(character) -- S'assurer que le Humanoid est prêt quand le personnage est ajouté
+end)
+
+local Slider = MainTab:CreateSlider({
+    Name = "walkspeed",
+    Range = {0, 300},
+    Increment = 1,
+    Suffix = " speed",
+    CurrentValue = 10,
+    Flag = "Slider1",
+    Callback = setWalkSpeed,
+})
+
+print("Slider created")
+
+
+print("Slider created")
+
+local Slider = MainTab:CreateSlider({
+   Name = "JumpPower",
+   Range = {0, 300},
+   Increment = 1,
+   Suffix = "jump",
+   CurrentValue = 10,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+   end,
+})
+
